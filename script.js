0e94460a1172bcd7a4fa6e9dcc91db059b220776
KrishnaNAcharya <knacharyakavoor@gmail.com>
2024-10-06 15:35:40 +0530

    added tailwind css and delete functionality

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// script.js
 function addTask() {
     var task = document.getElementById("taskInput").value;
     if (task === "") {
         alert("Please enter a task");
     } else {
         var li = document.createElement("li");
-        li.innerHTML = '<input type="checkbox" onclick="updateCount()">' + task;
+        li.className = "bg-[#DBBEA1]/10 p-2 rounded-lg flex items-center hover:bg-[#DBBEA1]/20 transition-colors";
+
+        li.innerHTML = '<input type="checkbox" onclick="updateCount()" class="mr-2 cursor-pointer"> ' + task +
+                       '<button onclick="deleteTask(this)" class="ml-auto bg-[#FF6B8B] px-2 py-1 rounded text-[#2A1D1E]">Delete</button>';
+
         document.getElementById("taskList").appendChild(li);
         document.getElementById("taskInput").value = "";
         updateCount();
@@ -26,4 +31,10 @@ function updateCount() {
 
     document.getElementById("completed").innerText = completed;
     document.getElementById("incomplete").innerText = incomplete;
+}
+
+function deleteTask(button) {
+    var li = button.parentElement;
+    li.remove();
+    updateCount();
 }
